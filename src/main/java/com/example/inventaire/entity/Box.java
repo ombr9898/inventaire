6232b6219b99121f3d0f8e8589ba8369c3597f7c
omar <vrtlvrtl89@gmail.com>
2022-10-24 01:35:48 +0200

    lombok

--- src/main/java/com/example/inventaire/entity/Box.java
+++ src/main/java/com/example/inventaire/entity/Box.java
@@ -3,13 +3,17 @@ package com.example.inventaire.entity;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 
+import java.util.TimeZone;
+
 public class Box {
     @Id
     @GeneratedValue
     private Long id;
-    private String name;
-
-    public Box( String name) {
-        this.name = name;
-    }
+    private String boxOfName;
+    private String location;
+    private String comment;
+    private String state;
+    private TimeZone dateOfCreation;
+    private TimeZone dateOfUpdated;
+    private int numberOfSample;
 }
