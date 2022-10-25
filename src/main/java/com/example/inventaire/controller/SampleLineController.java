786663d3b3f3131fa35589fd89ed10a709cc70a8
omar <vrtlvrtl89@gmail.com>
2022-10-25 15:17:16 +0200

    SampleLineController

--- src/main/java/com/example/inventaire/controller/SampleLineController.java
+++ src/main/java/com/example/inventaire/controller/SampleLineController.java
@@ -3,9 +3,7 @@ package com.example.inventaire.controller;
 import com.example.inventaire.entity.Sample;
 import com.example.inventaire.entity.SampleLine;
 import com.example.inventaire.service.implementation.SampleLineService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,6 +14,24 @@ public class SampleLineController {
     public SampleLineController(SampleLineService sampleLineService) {
         this.sampleLineService = sampleLineService;
     }
-    @GetMapping(value = "/sampleline/{sample}")
+    @GetMapping(value = "/samplelinebysample/{sample}")
     public List<SampleLine> getSampleLineOfSample(@PathVariable("sample") Sample sample){return sampleLineService.getSampleLineOfSample(sample);}
+    @GetMapping(value = "/sampleline/{id}")
+    public SampleLine getSampleLine(@PathVariable("id") Long id){return sampleLineService.getSampleLine(id);}
+    @GetMapping(value = "/sampleline")
+    public List<SampleLine> getAllSampleLine(){return sampleLineService.getAllSampleLines();}
+    @PostMapping(value = "/sampleline")
+    public  SampleLine addSampleLine(@RequestBody SampleLine sampleLine){return sampleLineService.addSampleLine(sampleLine);}
+
+    @PutMapping(value = "/sampleline")
+    public  SampleLine updateSampleLine(@RequestBody Long id,SampleLine sampleLine){return sampleLineService.updateSampleLine(id,sampleLine);}
+
+    @PostMapping(value="/takesample")
+            public List<SampleLine> takeSampleLines(List<SampleLine> sampleLineList){return sampleLineService.takeSampleLines(sampleLineList);}
+    @PostMapping(value = "returnsample")
+    public List<SampleLine> returnSampleLines(List<SampleLine> sampleLineList){return sampleLineService.returnSampleLines(sampleLineList);}
+
+    @DeleteMapping(value = "/sampleline/{id}")
+    String deleteSampleLine(@PathVariable("id") Long id){sampleLineService.deleteSampleLine(id); return "SampleLine deleted" ;}
+
 }
