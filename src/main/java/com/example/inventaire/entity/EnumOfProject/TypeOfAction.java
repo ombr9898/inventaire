b15bc7ce817503f72b0fd4c16ba5bc9a5f1c7492
omar <vrtlvrtl89@gmail.com>
2022-10-26 16:07:10 +0200

    Action Sample

--- src/main/java/com/example/inventaire/entity/EnumOfProject/TypeOfAction.java
+++ src/main/java/com/example/inventaire/entity/EnumOfProject/TypeOfAction.java
@@ -2,5 +2,5 @@ package com.example.inventaire.entity.EnumOfProject;
 
 public enum TypeOfAction {
 
-    TAKED,RETURNED,SHIPPED,MODIFIED,CREATED
+    TAKED,RETURNED,SHIPPED,MODIFIED,CREATED,DELETED
 }
--- src/main/java/com/example/inventaire/service/implementation/SampleLineService.java
+++ src/main/java/com/example/inventaire/service/implementation/SampleLineService.java
@@ -1,16 +1,22 @@
 package com.example.inventaire.service.implementation;
 
+import com.example.inventaire.entity.Action;
 import com.example.inventaire.entity.EnumOfProject.LocationSample;
+import com.example.inventaire.entity.EnumOfProject.SampleState;
+import com.example.inventaire.entity.EnumOfProject.TypeOfAction;
 import com.example.inventaire.entity.Sample;
 import com.example.inventaire.entity.SampleLine;
+import com.example.inventaire.repository.ActionRepository;
 import com.example.inventaire.repository.SampleLineRepository;
 import com.example.inventaire.service.contrat.SampleLineServiceContrat;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 @Service
 public class SampleLineService implements SampleLineServiceContrat {
     SampleLineRepository sampleLineRepository;
+    ActionRepository actionRepository;
 
     public SampleLineService(SampleLineRepository sampleLineRepository) {
         this.sampleLineRepository = sampleLineRepository;
@@ -18,6 +24,12 @@ public class SampleLineService implements SampleLineServiceContrat {
 
     @Override
     public SampleLine addSampleLine(SampleLine sampleLine) {
+        Action action=new Action();
+        List<SampleLine> sampleLineList=new ArrayList<>();
+        sampleLineList.add(sampleLine);
+        action.setListOfSampleLine(sampleLineList);
+        action.setTypeOfAction(TypeOfAction.CREATED);
+        actionRepository.save(action);
         return sampleLineRepository.save(sampleLine);
     }
 
@@ -32,6 +44,12 @@ public class SampleLineService implements SampleLineServiceContrat {
 
     @Override
     public SampleLine updateSampleLine(Long id, SampleLine sampleLine) {
+        Action action=new Action();
+        List<SampleLine> sampleLineList=new ArrayList<>();
+        sampleLineList.add(sampleLine);
+        action.setListOfSampleLine(sampleLineList);
+        action.setTypeOfAction(TypeOfAction.MODIFIED);
+        actionRepository.save(action);
         sampleLine.setId(id);
         return sampleLineRepository.save(sampleLine) ;
     }
@@ -48,6 +66,10 @@ public class SampleLineService implements SampleLineServiceContrat {
 
     @Override
     public List<SampleLine> takeSampleLines(List<SampleLine> sampleLineList) {
+        Action action=new Action();
+        action.setListOfSampleLine(sampleLineList);
+        action.setTypeOfAction(TypeOfAction.TAKED);
+        actionRepository.save(action);
         for (SampleLine sampleLine:
              sampleLineList) {
             sampleLine.setComment("Taked by User");
@@ -59,6 +81,10 @@ public class SampleLineService implements SampleLineServiceContrat {
 
     @Override
     public List<SampleLine> returnSampleLines(List<SampleLine> sampleLineList) {
+        Action action=new Action();
+        action.setListOfSampleLine(sampleLineList);
+        action.setTypeOfAction(TypeOfAction.RETURNED);
+        actionRepository.save(action);
         for (SampleLine sampleLine:
                 sampleLineList) {
             sampleLine.setComment("Returned by User");
@@ -72,7 +98,13 @@ public class SampleLineService implements SampleLineServiceContrat {
 
     @Override
     public Void deleteSampleLine(Long id) {
-        sampleLineRepository.deleteById(id);
+        Action action=new Action();
+        List<SampleLine> sampleLineList=new ArrayList<>();
+        sampleLineList.add(sampleLineRepository.findById(id).get());
+        action.setListOfSampleLine(sampleLineList);
+        action.setTypeOfAction(TypeOfAction.DELETED);
+        actionRepository.save(action);
+        sampleLineRepository.findById(id).get().setStateOfSampleLine(SampleState.DESTROYED);
 
         return null;
     }
